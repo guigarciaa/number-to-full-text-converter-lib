fd4680ab1c9aec05e0d00c22355cf46911380485
Guilherme Garcia <guigarcia.a@outlook.com>
2024-06-30 22:24:19 -0300

    feat: Add sample for using lib

---
+++ sample/Program.cs
@@ -0,0 +1,12 @@
+﻿using NumberToTextConverter;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var text_to_write = TextConverter.WriteLong(123456789.12M);
+        Console.WriteLine(text_to_write);
+        Console.WriteLine(JsonStringReader.ReadStringsJson());
+        Console.WriteLine("Press any key to exit...");
+    }
+}
